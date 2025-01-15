a5496b959d7896bfcb4dba6daf405d1f7f2b8e89
Yevgeniya Litvinova <3vlitv1@gmail.com>
2025-01-15 16:21:29 +0500

    All tasks are completed

--- litvinova/week1.py
+++ litvinova/week1.py
@@ -1,1 +1,81 @@
+#HOME
 print("Hello, World!")
+
+#Syntax
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Comments
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+z = "Python is awesome"
+print(z)
+
+glob = "awesome"
+
+def myfunc():
+    print("Python is " + glob)
+
+myfunc()
+
+#Data Types
+v = 5
+print(type(v))
+
+#Numbers
+i = 1    # int
+f = 2.8  # float
+com = 1j   # complex
+print(type(i))
+print(type(f))
+print(type(com))
+
+#Casting
+h = int(1)   # will be 1
+s = int(2.8) # will be 2
+k = int("3") # will be 3
+print(h)
+print(s)
+print(k)
+
+#Strings
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+str = "Hello, World!"
+print(str[2:5])
+
+str2 = "Hello, World!"
+print(str2.upper())
+
+subStr1 = "Hello"
+subStr2 = "World"
+str3 = subStr1 + subStr2
+print(str3)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt2 = "We are the so-called \"Vikings\" from the north."
+print(txt2)
